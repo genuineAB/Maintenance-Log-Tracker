68ddd3fcbefdca00664ee1239442f157ace77438
genuineAB <bamigboyeabiola@gmail.com>
2022-09-22 07:04:20 +0100

    Adjustment to Sign Up Page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,16 +7,27 @@ export const Home = () => {
     
     useEffect(() => {
         M.AutoInit()
-    })       
+    }) 
+
+    const onClick = (e) => {
+        e.preventDefault();
+        console.log("Register User")
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Log In User")
+    }
     
 
     return (
-    <body>
+    <div className='my-container'>
         <div className='header'>
             <h3><i className='fa-solid fa-screwdriver-wrench'> Maintenance Logger</i></h3>
             <p>Maintenance Logger helps you to effectively manage and track maintenance issues in your organization</p>
         </div>
-        <form className='form-1'>
+        <div  className='form-1'>
+            <form>
                 <div className="input-field col s6">
                     <input placeholder="Email Address" id="email" type="email" className="validate" />
                 </div>
@@ -24,84 +35,60 @@ export const Home = () => {
                     <input placeholder="Password" id="password" type="password" className="validate" />
                 </div>
                 <div>
-                    <button className="btn btn-1 waves-light" type="submit" name="action">Sign In
-                    {/* <i className="material-icons right">send</i> */}
+                    <button className="btn btn-1 waves-light" type="submit" name="action" onClick={onSubmit}>Sign In
                     </button>
                 </div>
                 <div className='recover'>
                         <a href='!#'> Forgotten Password?</a>
                 </div>
-                
-                <div className='btn-items'>
-                    {/* <button className="btn waves-effect waves-light btn-2" type="submit" name="action" onSubmit={onClick}>Create New Account
-                    </button> */}
-                    <button className="waves-effect waves-light btn modal-trigger btn-2" href="#modal1">Create New Account</button>
-
-                    <div id="modal1" className="modal form-1">
-                        <div className="modal-content">
-                            <h4>Sign Up</h4>
-                            <p>It's Quick and Easy</p>
-                        </div>
-
-                        <div className="row">
-                            <form className="col s12">
-                                <div className="row">
-                                    <div className="input-field col s6">
-                                    <input placeholder="Full Name" id="full_name" type="text" className="validate" />
-                                    <label for="full_name"> Full Name</label>
-                                    </div>
-                                    <div className="input-field col s6">
-                                    <input id="organization_name" type="text" className="validate" />
-                                    <label for="organization_name">Organization Name</label>
-                                    </div>
-                                </div>
-                                    <div className="row">
-                                    <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate" />
-                                    <label for="email">Email</label>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="input-field col s12">
-                                    <input id="password" type="password" className="validate" />
-                                    <label for="password">Password</label>
-                                    </div>
-                                </div>
-                            
-                                <div className="modal-footer">
-                                    <button href="#!" className="modal-close waves-effect waves-green btn-flat btn-2">Sign Up</button>
-                                    <button href="#!" className="modal-close waves-effect waves-green btn-flat btn-2">Close</button>
-                                </div>
-                            </form>
-                        </div>
-                        {/* <form >
-                            <div className="input-field col s6">
-                                <input placeholder="Full Name" id="name" type="name" className="validate" />
-                                
-                            </div>
-                            
-                            <div className="input-field col s6">
-                                <input placeholder="Email" id="email" type="email" className="validate" />
-                            </div>
-                            <div className="input-field col s6">
-                                <input placeholder="Password" id="password" type="password" className="validate" />
-                            </div>
-                            <div className="input-field col s6">
-                                <input placeholder="Organization Name" id="organization" type="name" className="validate" />
-                            </div>
-                            <div className="modal-footer">
-                            <button href="#!" className="modal-close waves-effect waves-green btn-flat btn-2">Sign Up</button>
-                            <button href="#!" className="modal-close waves-effect waves-green btn-flat btn-2">Close</button>
-                        </div>
-                        </form> */}
-                        
-                    </div>
-                </div>
             </form>
 
-            
+            <div className='btn-items'>
+                <button className="waves-effect waves-light btn modal-trigger btn-2" href="#modal1">Create New Account</button>
+
+                <div id="modal1" className="modal form-2">
+                    <div className="modal-content">
+                        <h4>Sign Up</h4>
+                        <p>It's Quick and Easy</p>
+                    </div>
+
+                    <div className="row">
+                        <form className="col s12">
+                            <div className="row">
+                                <div className="input-field col s6">
+                                <input id="full_name" type="text" className="validate" />
+                                <label htmlFor="full_name"> Full Name</label>
+                                </div>
+                                <div className="input-field col s6">
+                                <input id="organization_name" type="text" className="validate" />
+                                <label htmlFor="organization_name">Organization Name</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <input id="email_2" type="email" className="validate" />
+                                <label htmlFor="email">Email</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <input id="password_2" type="password" className="validate" />
+                                <label htmlFor="password">Password</label>
+                                </div>
+                            </div>
+                            
+                            <div className="modal-footer submit-btn">
+                                <button href="#!" className="modal-close waves-effect btn-flat submit-1" onClick={onClick}>Sign Up</button>
+                                <button href="#!" className="modal-close waves-effect btn-flat submit-2">Close</button>
+                            </div>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
         
         
-    </body>
+    </div>
   )
 }
