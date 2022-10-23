e2ccd31c9d9df597f67b73f4b978cfbad3cb6aa0
genuineAB <bamigboyeabiola@gmail.com>
2022-10-23 11:17:15 +0100

    Further Changes Made for Deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-// import * as React from 'react';
-// import {useEffect, Fragment} from 'react';
-// import './App.css';
-// import Pages from './pages';
-// import 'materialize-css/dist/css/materialize.min.css';
-// import M from 'materialize-css/dist/js/materialize.min.js';
-// import { Provider } from 'react-redux';
-// import store from './store';
+import * as React from 'react';
+import {useEffect, Fragment} from 'react';
+import './App.css';
+import Pages from './pages';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
-// const App = () => {
-//   useEffect(() => {
-//     //Initialize materialize JS
-//     M.AutoInit();
-//   })
-//   return (
-//     <Provider store={store}>
-//         <Fragment>
-//             <Pages />
-//         </Fragment>
+const App = () => {
+  useEffect(() => {
+    //Initialize materialize JS
+    M.AutoInit();
+  })
+  return (
+    <Provider store={store}>
+        <Fragment>
+            <Pages />
+        </Fragment>
       
-//     </Provider>
+    </Provider>
   
-//   )
-// }
+  )
+}
 
-// export default App;
+export default App;
