049a34ef5dd8f0e2cb78eaf1a0168e11fe0cad45
genuineAB <bamigboyeabiola@gmail.com>
2022-10-24 22:50:16 +0100

    Changed File Structure

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = config.get('mongoURI');
+const db = "mongodb+srv://abLogs123:abLogs123@maintenancelogkeeper.5tybs4o.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
     try {
